ddbf84f409444a0864eb68ea35f4779b9b7c13e3
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-19 11:06:27 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,22 @@
+const someRandomVariable = 3 ; 
+let someOtherRandomVariable = 2 ; 
+
+console.log('SUM: ',someRandomVariable+someOtherRandomVariable );
+console.log('SUB: ',someRandomVariable-someOtherRandomVariable );
+console.log('MUL: ',someRandomVariable*someOtherRandomVariable );
+console.log('DIV: ',someRandomVariable/someOtherRandomVariable );
+console.log('MOD: ',someRandomVariable%someOtherRandomVariable )
+console.log('POW: ',someRandomVariable**someOtherRandomVariable );
+
+console.log("Increment: ",++someOtherRandomVariable)
+console.log("Decreament: ",--someOtherRandomVariable)
+
+console.log('Comparision Opearator : ',someRandomVariable>=someOtherRandomVariable);
+console.log('Comparision Opearator : ',someRandomVariable<=someOtherRandomVariable);
+console.log('Comparision Opearator : ',someRandomVariable==someOtherRandomVariable);
+console.log('Comparision Opearator : ',someRandomVariable!=someOtherRandomVariable);
+
+
+
+const result = 20 + 50/2;
+ console.log(result);
