b039627812164d2d64191773e2cd43520499c5ad
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-19 10:48:12 +0530

    varible in js

---
+++ variable.js
@@ -0,0 +1,14 @@
+const stringVariable= 'hello world ';
+const numberVariable = 20;
+const booleanVariable = true ; 
+const myArray = [1,2,3,4,5];
+
+const myObject = {
+property1:'some value1',
+property2:'some value1',
+property3:'some value1'
+}
+console.log(stringVariable);
+console.log(booleanVariable);
+console.log(myArray[0]);
+console.log(myObject.property1);
